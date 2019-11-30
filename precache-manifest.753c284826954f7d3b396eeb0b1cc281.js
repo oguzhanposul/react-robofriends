597dd87fb721a31f0e6f278c5961265c057e5877
oguzhan posul <oguzhan.posul@C02Z65XHLVDQ.local>
2019-11-30 15:52:18 +0300

    Updates

---
+++ precache-manifest.753c284826954f7d3b396eeb0b1cc281.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d9822b297e6dcf1b3ce99f9f8168ab33",
+    "url": "/react-robofriends/index.html"
+  },
+  {
+    "revision": "a894afa21c3391613dcc",
+    "url": "/react-robofriends/static/css/2.eb45ad44.chunk.css"
+  },
+  {
+    "revision": "c728c9c775cb14661577",
+    "url": "/react-robofriends/static/css/main.34de6062.chunk.css"
+  },
+  {
+    "revision": "a894afa21c3391613dcc",
+    "url": "/react-robofriends/static/js/2.954ab8f1.chunk.js"
+  },
+  {
+    "revision": "c728c9c775cb14661577",
+    "url": "/react-robofriends/static/js/main.45a6bbaf.chunk.js"
+  },
+  {
+    "revision": "8f2d44ca28f742a96e01",
+    "url": "/react-robofriends/static/js/runtime-main.33eb9e57.js"
+  }
+]);
